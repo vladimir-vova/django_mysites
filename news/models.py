34a71b52af7a0335237dcacd665402aef42283de
vladimir-vova <baimieff.volodya1@yandex.ru>
2020-11-11 23:35:08 +0500

    I was adding moduls

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_ad = models.DateTimeField(auto_now_add=True)
+    updated_ad = models.DateTimeField(auto_noew=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from news.views import *
+from .views import *
 
 app_name = "news"
 
