046c5cd57c203cd3351a7e5974072b92c1e2a9d1
vladimir-vova <baimieff.volodya1@yandex.ru>
2020-11-13 19:59:28 +0500

    It is a module

--- news/models.py
+++ news/models.py
@@ -5,6 +5,6 @@ class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     created_ad = models.DateTimeField(auto_now_add=True)
-    updated_ad = models.DateTimeField(auto_noew=True)
+    updated_ad = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     is_published = models.BooleanField(default=True)
