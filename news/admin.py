d5723a5f976a881177779b94d430e55bf74f13a5
vladimir-vova <baimieff.volodya1@yandex.ru>
2020-11-15 22:28:49 +0500

    It is for AdminDjango

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id','title','content','created_ad','updated_ad','is_published')
+    list_display_links = ('id','title')
+    search_fields = ('title','content')
+
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_ad = models.DateTimeField(auto_now_add=True)
-    updated_ad = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        
+    title = models.CharField(max_length=150,verbose_name='Наименование')
+    content = models.TextField(blank=True,verbose_name='Содержание')
+    created_ad = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+    updated_ad = models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',verbose_name='Фото',blank=True)
+    is_published = models.BooleanField(default=True,verbose_name='Опубликован')
     
     def __str__(self):
         return self.title
