8e913c324967f0eb8e03fb8333df32552726fd57
vladimir-vova <baimieff.volodya1@yandex.ru>
2020-11-15 23:01:52 +0500

    It is a models and a admin

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id','title','content','created_ad','updated_ad','is_published')
+    list_display = ('id','title','category','content','created_ad','updated_ad','is_published')
     list_display_links = ('id','title')
     search_fields = ('title','content')
+    list_editable = ('is_published',)
+    list_filter = ('title','is_published')
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id','title')
+    search_fields = ('title',)
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- news/models.py
+++ news/models.py
@@ -12,6 +12,18 @@ class News(models.Model):
     updated_ad = models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/',verbose_name='Фото',blank=True)
     is_published = models.BooleanField(default=True,verbose_name='Опубликован')
+    category = models.ForeignKey('Category',on_delete=models.PROTECT,null=True,verbose_name='Категория')
     
+    
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        
+    title = models.CharField(max_length=150,db_index=True,verbose_name='Наименование категории')
+        
     def __str__(self):
         return self.title
