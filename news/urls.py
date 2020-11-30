8c3e29d2edf1032008254161e79246cfc5b150f7
vladimir-vova <baimieff.volodya1@yandex.ru>
2020-11-30 21:44:47 +0500

    Это ListView

--- news/urls.py
+++ news/urls.py
@@ -4,8 +4,10 @@ from .views import *
 app_name = "news"
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('category/<int:category_id>/', get_category, name="category"),
+    #path('', index, name="index"),
+    path('', HomeNews.as_view(), name="index"),
+    #path('category/<int:category_id>/', get_category, name="category"),
+    path('category/<int:category_id>/', NewsByCategory.as_view(), name="category"),
     path('news/<int:news_id>/', view_news, name="view_news"),
     path('news/add-news/', add_news, name="add_news"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 
 from .models import News, Category
 from .forms import NewsForm
 
-def index(request):
-    news = News.objects.all()
-    args = {
-        'news':news,
-    }
-    return render(request,'news/index.html',args)
+class HomeNews(ListView):
+    model = News #аналог news = News.objects.all()
+    template_name = 'news/index.html'
+    context_object_name = 'news'
     
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+
+#def index(request):
+#    news = News.objects.all()
+#    args = {
+#        'news':news,
+#    }
+#    return render(request,'news/index.html',args)
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/category.html'
+    context_object_name = 'news'
+    allow_empty = False
+    
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+    
+    
+#def get_category(request, category_id):
+#    news = News.objects.filter(category_id=category_id)
     # category = Category.objects.get(pk=category_id)
-    category = get_object_or_404(Category, pk=category_id)
-    args = {
-        'news':news,
-        'category': category,
-    }
-    return render(request,'news/category.html',args)
+#    category = get_object_or_404(Category, pk=category_id)
+#    args = {
+#        'news':news,
+#        'category': category,
+#    }
+#    return render(request,'news/category.html',args)
     
 def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
