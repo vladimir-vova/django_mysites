b6eac0f5c2099d5cd4ffa01a432c1b7bc1899556
vladimir-vova <baimieff.volodya1@yandex.ru>
2020-11-30 22:03:45 +0500

    Это DetailView

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import News, Category
 from .forms import NewsForm
@@ -39,14 +39,21 @@ class NewsByCategory(ListView):
 #        'category': category,
 #    }
 #    return render(request,'news/category.html',args)
+
+class ViewNews(DetailView):
+    model = News
+    # pk_url_kwarg = 'news_id'
+    template_name = 'news/view_news.html'
+    context_object_name = 'news_item'
     
-def view_news(request, news_id):
+    
+#def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    args = {
-        'news_item': news_item,
-    }
-    return render(request,'news/view_news.html', args)
+#    news_item = get_object_or_404(News, pk=news_id)
+#    args = {
+#        'news_item': news_item,
+#    }
+#    return render(request,'news/view_news.html', args)
     
 def add_news(request):
     if request.method == 'POST':
